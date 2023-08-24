e62ea8e07dcfc0c696ffec879d8424a84fdf996e
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-24 10:47:31 +0000

    read url and json

---
+++ Controller/AllController.py
@@ -0,0 +1,18 @@
+import requests
+
+class ReadUrl:
+
+  def read_json(self, url):
+    response = requests.get(url)
+    if response.status_code == 200:
+      return response.json()
+    return None
+
+
+class COLLEGE:
+
+  def CollegeList(self):
+    reader = ReadUrl()
+    college_list = reader.read_json(
+      'https://api-frontend.kemdikbud.go.id/loadpt')
+    return college_list
