f18cecbc90d29845f40ee548ad0bbb7c50efcd4e
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-24 10:48:14 +0000

    add route

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from Controller.AllController import COLLEGE
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('dashboard.html')
 
-@app.route('/hehe')
-def hehe():
-  return "ini putri amanda K"
+@app.route("/colleges")
+def colleges():
+  colleges = COLLEGE()
+  college_list = colleges.CollegeList()
+  return render_template("colleges.html", college_list=college_list)
